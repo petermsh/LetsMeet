83691b3a52611a9ffe2281fab99c42013d0ff644
Piotr <aron17101@gmail.com>
2024-12-02 16:11:20 +0100

    Added configurations

---
+++ LetsMeet.Api/Program.cs
@@ -0,0 +1,14 @@
+using LetsMeet.Application;
+using LetsMeet.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddApplication()
+    .AddInfrastructure(builder.Configuration);
+
+var app = builder.Build();
+
+app.UseInfrastructure();
+
+app.Run();
---
+++ LetsMeet.Infrastructure/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LetsMeet.Infrastructure;
+
+public static class Extensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        services.AddControllers();
+        
+        return services;
+    }
+
+    public static WebApplication UseInfrastructure(this WebApplication app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        app.MapControllers();
+
+        return app;
+    }
+}
